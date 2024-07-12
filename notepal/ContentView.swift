7e76d5c9d7f80e5f2657a742f9557a083e69a469
Ahmad Naufal Alfakhar <direct.anaufal@gmail.com>
2024-07-12 15:15:03 +0700

    Initial Commit

---
+++ notepal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notepal
+//
+//  Created by ahmad naufal alfakhar on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notepal/notepalApp.swift
@@ -0,0 +1,17 @@
+//
+//  notepalApp.swift
+//  notepal
+//
+//  Created by ahmad naufal alfakhar on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct notepalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
