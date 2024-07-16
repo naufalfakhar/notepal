d7643dcd984d29c12c3d90147a81bbfcc2c34b37
HansenYudistira <126214334+HansenYudistira@users.noreply.github.com>
2024-07-16 22:46:30 +0700

    feat: add daily quest view

---
+++ notepal/Views/Habit/DailyQuestView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyQuestView.swift
+//  notepal
+//
+//  Created by Hansen Yudistira on 16/07/24.
+//
+
+import SwiftUI
+
+struct DailyQuestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyQuestView()
+}
